2659777820fa98c4202ac7c8537ffb54d98a738a
Rahul <rahulsept2191@gmail.com>
2018-08-08 00:22:40 +0530

    Fixed

--- identity.api/lib/database/user.js
+++ identity.api/lib/database/user.js
@@ -5,5 +5,19 @@ export default {
     emailAddress: 'dummy',
     token: 'dummy',
     password: 'dummy'
+  }, {
+    id: 8,
+    username: 'rahul.singh4',
+    personId: '08105db0-4935-4c25-b1e3-ce1f5eb2a3a0',
+    emailAddress: 'f@g.com',
+    token: '4aa41c8c-15f3-23c4-912a-cc528e462bc4',
+    password: 'identity'
+  }, {
+    id: 3,
+    username: 'd',
+    personId: '50f22539-d778-4566-bf90-8a495463936a',
+    emailAddress: 'f@g.com',
+    token: 'a815a448-513a-4dfb-a3b6-69784ce66cc3',
+    password: 'identity'
   }]
 }
